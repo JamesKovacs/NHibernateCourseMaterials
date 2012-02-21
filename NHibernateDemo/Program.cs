a6ba304e156bd6d2f882535fd8ba3e8fdd7515d2
James Kovacs <jkovacs@post.harvard.edu>
2012-02-20 18:13:37 -0700

    Mapped additional Customer properties and ShippingMethod

--- NHibernateDemo/Program.cs
+++ NHibernateDemo/Program.cs
@@ -29,16 +29,31 @@ namespace NHibernateDemo
 
 //            cfg.GetClassMapping(typeof (ShippingMethod)).IsMutable = true;
 
+            int newId;
             var sessionFactory = cfg.BuildSessionFactory();
             using(var session = sessionFactory.OpenSession())
             using(var tx = session.BeginTransaction())
             {
-//                var customer = new Customer { Name = "John Smith"};
-//                session.Save(customer);
+                var customer = new Customer { Name = "John Smith", 
+                    MemberSince = DateTimeOffset.Now, 
+                    Rating=2d/3};
+                Console.WriteLine("Original:");
+                Console.WriteLine(customer);
+                session.Save(customer);
+                newId = customer.Id;
 //                var customer = session.Load<Customer>(1);
 //                Console.WriteLine(customer);
-                var shippingMethod = new ShippingMethod {Name = "Priority"};
-                session.Save(shippingMethod);
+//                var shippingMethod = new ShippingMethod {Name = "Priority"};
+//                session.Save(shippingMethod);
+                tx.Commit();
+            }
+
+            using(var session = sessionFactory.OpenSession())
+            using (var tx = session.BeginTransaction())
+            {
+                var customer = session.Load<Customer>(newId);
+                Console.WriteLine("Reloaded:");
+                Console.WriteLine(customer);
                 tx.Commit();
             }
             Console.WriteLine("Press <ENTER> to continue...");
@@ -48,12 +63,20 @@ namespace NHibernateDemo
 
     public class Customer
     {
+        public Customer()
+        {
+            MemberSince = new DateTime(2000, 1, 1);
+        }
+
         public virtual int Id { get; set; }
         public virtual string Name { get; set; }
+        public virtual DateTimeOffset MemberSince { get; set; }
+        public virtual bool IsGoldMember { get; set; }
+        public virtual double Rating { get; set; }
 
         public override string ToString()
         {
-            return string.Format("Id: {0}, Name: {1}", Id, Name);
+            return string.Format("Id: {0}, Name: {1}, MemberSince: {2}, IsGoldMember: {3}, Rating: {4}", Id, Name, MemberSince, IsGoldMember, Rating);
         }
     }
 
