06f2325b8454f7f1c9aa61a85d1dac63f8c6d8ff
James Kovacs <jkovacs@post.harvard.edu>
2012-02-20 18:12:47 -0700

    Configuration demos

--- NHibernateDemo/Program.cs
+++ NHibernateDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using HibernatingRhinos.Profiler.Appender.NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
@@ -10,23 +11,34 @@ namespace NHibernateDemo
     {
         static void Main()
         {
+            NHibernateProfiler.Initialize();
+
             var cfg = new Configuration();
+            cfg.Configure(); // OPTIONAL: Only required if you want to read in hibernate.cfg.xml
             cfg.DataBaseIntegration(x =>
                                         {
                                             x.ConnectionStringName = "medassets";
                                             x.Driver<SqlClientDriver>();
                                             x.Dialect<MsSql2008Dialect>();
                                             x.IsolationLevel = IsolationLevel.ReadCommitted;
+                                            x.Timeout = 10;
                                         });
+            cfg.SessionFactoryName("MedAssets");
+            cfg.SessionFactory().GenerateStatistics();
             cfg.AddAssembly(typeof (Customer).Assembly);
+
+//            cfg.GetClassMapping(typeof (ShippingMethod)).IsMutable = true;
+
             var sessionFactory = cfg.BuildSessionFactory();
             using(var session = sessionFactory.OpenSession())
             using(var tx = session.BeginTransaction())
             {
 //                var customer = new Customer { Name = "John Smith"};
 //                session.Save(customer);
-                var customer = session.Load<Customer>(1);
-                Console.WriteLine(customer);
+//                var customer = session.Load<Customer>(1);
+//                Console.WriteLine(customer);
+                var shippingMethod = new ShippingMethod {Name = "Priority"};
+                session.Save(shippingMethod);
                 tx.Commit();
             }
             Console.WriteLine("Press <ENTER> to continue...");
@@ -44,4 +56,11 @@ namespace NHibernateDemo
             return string.Format("Id: {0}, Name: {1}", Id, Name);
         }
     }
+
+    public class ShippingMethod
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+    }
 }
+
