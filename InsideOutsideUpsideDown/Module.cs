02c9ddd20497cf7f662c459e7d2da9e1e2003c3d
James Kovacs <jkovacs@post.harvard.edu>
2012-02-22 17:10:15 -0700

    Lazy loaded properties proxy issue

---
+++ InsideOutsideUpsideDown/Module.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InsideOutsideUpsideDown
+{
+    public class Module
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string ModuleCode { get; set; }
+
+        private string moduleXml;
+        public virtual string ModuleXml
+        {
+            get
+            {
+                if (moduleXml == null)
+                {
+                    moduleXml = Parse(ModuleCode);
+                }
+                return moduleXml;
+            }
+        }
+
+        private string Parse(string moduleCode)
+        {
+            // Faking out some XML
+            return "<root>" + moduleCode + "</root>";
+        }
+    }
+}
---
+++ InsideOutsideUpsideDown/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+
+namespace InsideOutsideUpsideDown
+{
+    class Program
+    {
+        static void Main()
+        {
+            NHibernateProfiler.Initialize();
+
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+                                        {
+                                            x.ConnectionStringName = "scratch";
+                                            x.Driver<SqlClientDriver>();
+                                            x.Dialect<MsSql2008Dialect>();
+                                        });
+            cfg.SessionFactory().GenerateStatistics();
+            cfg.AddAssembly(typeof (Program).Assembly);
+
+            var exporter = new SchemaExport(cfg);
+            exporter.Execute(false, true, false);
+
+            var sessionFactory = cfg.BuildSessionFactory();
+            using(var session = sessionFactory.OpenSession())
+            using(var tx = session.BeginTransaction())
+            {
+                var module = new Module {ModuleCode = "class Foo; end;"};
+                session.Save(module);
+                tx.Commit();
+            }
+            using(var session = sessionFactory.OpenSession())
+            using(var tx = session.BeginTransaction())
+            {
+                var module = session.QueryOver<Module>().List().First();
+//                Console.WriteLine(module.ModuleCode);
+                Console.WriteLine(module.ModuleXml);
+                tx.Commit();
+            }
+            Console.WriteLine("Press <ENTER> to exit...");
+            Console.ReadLine();
+        }
+    }
+}
+
